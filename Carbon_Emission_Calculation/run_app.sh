95768cc0f90d11c4377c2e828cae1712580b433e
Allenzhouyujia <138656533+Allenzhouyujia@users.noreply.github.com>
2025-11-23 22:28:36 -0800

    Delete Carbon_Emission_Calculation directory

--- Carbon_Emission_Calculation/run_app.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "Starting Image Normalization Tool..."
-echo "Checking for required packages..."
-pip3 install -r requirements.txt
-echo "Starting Application..."
-python3 image_normalization_demo.py
